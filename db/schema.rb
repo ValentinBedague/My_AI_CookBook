add4ed5d355fc40b38fc7f9fbc841a51acf9e981
remi bostvironnois <remibostvironnois@gmail.com>
2025-06-10 12:58:41 +0200

    all models generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_10_105413) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_105750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_messages", force: :cascade do |t|
+    t.string "role"
+    t.text "content"
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_chat_messages_on_chat_id"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.string "title"
+    t.bigint "recipe_id", null: false
+    t.string "model_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_chats_on_recipe_id"
+  end
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
@@ -94,6 +112,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_10_105413) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chat_messages", "chats"
+  add_foreign_key "chats", "recipes"
   add_foreign_key "displayed_ingredients", "recipes"
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "messages", "recipes"
