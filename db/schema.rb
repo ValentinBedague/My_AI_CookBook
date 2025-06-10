0c1315e608b9120e655d2a919e9f43a2cf672c11
remi bostvironnois <remibostvironnois@gmail.com>
2025-06-10 12:50:49 +0200

    add message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_10_104510) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_105010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_10_104510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "role"
+    t.text "content"
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_messages_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -71,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_10_104510) do
 
   add_foreign_key "displayed_ingredients", "recipes"
   add_foreign_key "ingredients", "recipes"
+  add_foreign_key "messages", "recipes"
   add_foreign_key "recipes", "users"
   add_foreign_key "restrictions", "users"
 end
