33ea86c36b67ef046947490f32d9b45aedd8fcc3
remi bostvironnois <remibostvironnois@gmail.com>
2025-06-10 12:47:24 +0200

    add ingredient et diplayed ingredient model

---
+++ app/models/displayed_ingredient.rb
@@ -0,0 +1,3 @@
+class DisplayedIngredient < ApplicationRecord
+  belongs_to :recipe
+end
---
+++ test/models/displayed_ingredient_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DisplayedIngredientTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
