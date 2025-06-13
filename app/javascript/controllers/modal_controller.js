4bc4732b54b61c37662c6f459e9eb4496590b980
manuelle augereau <manuelleaugereau@gmail.com>
2025-06-13 15:32:57 +0200

    enable scroll closing modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -10,19 +10,18 @@ export default class extends Controller {
     const close = document.querySelector("#close")
 
     dots.addEventListener("click", (event) => {
-
-    // Code JS pour cette page uniquement
-    event.preventDefault();
-    modal.classList.remove("d-none");
-    modal.setAttribute("style", "position: fixed; bottom:60px;");
-    container.setAttribute("style", "filter: blur(4px);");
-    document.body.classList.add('no-scroll');
+      event.preventDefault();
+      modal.classList.remove("d-none");
+      modal.setAttribute("style", "position: fixed; bottom:60px;");
+      container.setAttribute("style", "filter: blur(4px);");
+      document.body.classList.add('no-scroll');
     });
 
     close.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.classList.add("d-none");
-    container.setAttribute("style", "filter: blur(0px);");
+      event.preventDefault();
+      modal.classList.add("d-none");
+      container.setAttribute("style", "filter: blur(0px);");
+      document.body.classList.remove('no-scroll');
   });
   }
 }
