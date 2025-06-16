6b693812882f76346fb9e6ddb33bf7f1e3cfd459
remi bostvironnois <remibostvironnois@gmail.com>
2025-06-16 16:43:24 +0200

    correction de bugs

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -12,7 +12,6 @@ export default class extends Controller {
     dots.addEventListener('click', (event) => {
       event.preventDefault();
       modal.classList.remove('d-none');
-      this.okButtonTarget.classList.remove('d-none')
       modal.setAttribute('style', 'position: fixed; bottom:60px;');
       container.setAttribute('style', 'filter: blur(4px);');
       document.body.classList.add('no-scroll');
@@ -20,7 +19,6 @@ export default class extends Controller {
     close.addEventListener('click', (event) => {
       event.preventDefault();
       modal.classList.add('d-none');
-      this.okButtonTarget.classList.add('d-none')
       container.setAttribute('style', 'filter: blur(0px);');
       document.body.classList.remove('no-scroll');
     });
@@ -29,6 +27,9 @@ export default class extends Controller {
     this.minusIconTargets.forEach((icon) => {
       icon.classList.remove('d-none');
     });
+    if (this.hasOkButtonTarget) {
+      this.okButtonTarget.classList.remove("d-none")
+    }
     this.modalHideTarget.classList.add('d-none');
     this.blurredTarget.setAttribute('style', 'filter: blur(0px);');
     document.body.classList.remove('no-scroll');
@@ -37,7 +38,9 @@ export default class extends Controller {
     this.minusIconTargets.forEach((icon) => {
       icon.classList.add('d-none');
     });
-    this.okButtonTarget.classList.add('d-none')
+    if (this.hasOkButtonTarget) {
+      this.okButtonTarget.classList.add("d-none")
+    }
 
   }
 }
