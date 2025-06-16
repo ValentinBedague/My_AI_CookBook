bcb71ce4ff2b31299b6fad01bbe1b7e47a84c527
manuelle augereau <manuelleaugereau@gmail.com>
2025-06-16 16:35:41 +0200

    Model

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -1,3 +1,18 @@
 class Restriction < ApplicationRecord
   belongs_to :user
+
+  AVAILABLE_DIETS = [
+    ['Végétarien', 'vegetarian'],
+    ['Végétalien', 'vegan'],
+    ['Sans gluten', 'gluten_free'],
+    ['Sans lactose', 'dairy_free'],
+    ['Sans noix', 'nut_free'],
+    ['Halal', 'halal'],
+    ['Casher', 'kosher']
+  ].freeze
+
+  # Initialiser diet comme un array vide
+  def diet
+    super || []
+  end
 end
