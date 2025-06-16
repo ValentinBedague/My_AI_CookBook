dfc5a763c71b7b9aa3842c263b89fc25565a07c2
remi bostvironnois <remibostvironnois@gmail.com>
2025-06-16 15:49:40 +0200

    edit fini

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,28 +1,41 @@
-import { Controller } from "@hotwired/stimulus"
-
+import { Controller } from '@hotwired/stimulus';
 // Connects to data-controller="modal"
 export default class extends Controller {
+  static targets = ['minusIcon', 'modalHide', 'blurred','okButton'];
   connect() {
-
-    const dots = document.querySelector(".dots");
-    const modal = document.querySelector(".modal-options");
-    const container = document.querySelector(".container")
-    const close = document.querySelector("#close")
-
-    dots.addEventListener("click", (event) => {
-
-    // Code JS pour cette page uniquement
-    event.preventDefault();
-    modal.classList.remove("d-none");
-    modal.setAttribute("style", "position: fixed; bottom:60px;");
-    container.setAttribute("style", "filter: blur(4px);");
-    document.body.classList.add('no-scroll');
+    // this.modalHideTarget.classList.add('d-none');
+    const dots = document.querySelector('.dots');
+    const modal = document.querySelector('.modal-options');
+    const container = document.querySelector('.container');
+    const close = document.querySelector('#close');
+    dots.addEventListener('click', (event) => {
+      event.preventDefault();
+      modal.classList.remove('d-none');
+      this.okButtonTarget.classList.remove('d-none')
+      modal.setAttribute('style', 'position: fixed; bottom:60px;');
+      container.setAttribute('style', 'filter: blur(4px);');
+      document.body.classList.add('no-scroll');
     });
-
-    close.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.classList.add("d-none");
-    container.setAttribute("style", "filter: blur(0px);");
-  });
+    close.addEventListener('click', (event) => {
+      event.preventDefault();
+      modal.classList.add('d-none');
+      this.okButtonTarget.classList.add('d-none')
+      container.setAttribute('style', 'filter: blur(0px);');
+      document.body.classList.remove('no-scroll');
+    });
+  }
+  remove() {
+    this.minusIconTargets.forEach((icon) => {
+      icon.classList.remove('d-none');
+    });
+    this.modalHideTarget.classList.add('d-none');
+    this.blurredTarget.setAttribute('style', 'filter: blur(0px);');
+    document.body.classList.remove('no-scroll');
+  }
+  ok() {
+    this.minusIconTargets.forEach((icon) => {
+      icon.classList.add('d-none');
+    });
+    this.okButtonTarget.classList.add('d-none')
   }
 }
